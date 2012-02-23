2091101a957e9c188b6addc59a8c503a171735af
Moritz Lawitschka <moritz@urbanvention.com>
2012-02-23 15:12:14 +0100

    created empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarsFovCalc::Application.config.secret_token = '5fbdf286f3b2df0a99b96fd8580fca49e9935f8a44d781566feb57c787f50ec5cd18b0701bc671d2890c764119af979d4381847b48c9663d3fbe270e59c26efd'
